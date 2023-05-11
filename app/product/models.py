20d19eef6810eb835d288bc18b7d0c6476f04390
ridwanray <alabarise@gmail.com>
2023-05-11 17:51:03 +0100

    modify product models

--- app/product/models.py
+++ app/product/models.py
@@ -3,6 +3,5 @@ from django.db import models
 
 
 class Product(AuditableModel):
-    name = models.DecimalField(max_digits=8, decimal_places=2)
-    description =  models.DateTimeField(auto_now_add=True)
-    price = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
+    name = models.CharField(max_length=50)
+    description =  models.TextField(blank=True, null=True)
